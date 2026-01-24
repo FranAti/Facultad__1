a1da3d60bdb630009bb3c4afcf87cac1a4a5bf75
Fran <fraativil@alu.edu.gva.es>
2026-01-24 14:08:54 +0100

    1.1

--- src/model/PersonalServicio.java
+++ src/model/PersonalServicio.java
@@ -1,5 +1,11 @@
 package model;
 
+import java.time.LocalDate;
+
 public class PersonalServicio extends Empleado {
     private Secc seccion;
+    public PersonalServicio(String nombre, String apellido, String dni, Est_civ estado, int num_despacho, LocalDate fecha_incorporacion, Secc seccion) {
+        super(nombre, apellido, dni, estado, num_despacho, fecha_incorporacion);
+        this.seccion = seccion;
+    }
 }
